612ad8fadfd44518c8ecc8d039bcbe5425e3edaf
simon.du <a932455223@qq.com>
2017-01-24 23:15:29 +0800

    update

--- dom.js
+++ dom.js
@@ -20,3 +20,22 @@ cssVendor=function(){
                 _prop=camelCase(cssVendor+prop);
             return (prop in divstyle) && prop || (_prop in divstyle) && _prop || '';
         }
+        function getStyle(elem,prop){
+                var style=document.getComputedStyle&&document.getComputedStyle(elem,null)||elem.currentStyle||elem.style;
+                return style[prop];
+            }
+            function setStyle(elem,props){
+                    each(props,function(name,value){
+                        var prop;
+                        switch(name){
+                            case 'float':
+                                prop=cssTest('cssFloat')?'cssFloat':'styleFloat';
+                                break;
+                            default:
+                                prop=camelCase(name);
+                        }
+                        try{
+                            elem.style[prop]=value;
+                        }catch(e){}
+                    });
+                }
