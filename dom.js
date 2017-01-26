49437b57d4626e5df2cb5b9db72b23508ff4ed3a
simon.du <a932455223@qq.com>
2017-01-26 23:07:58 +0800

    add underscore throttle

--- dom.js
+++ dom.js
@@ -18,6 +18,7 @@ cssVendor=function(){
     function EASE(t,b,c,d){
            return -c * ((t=t/d-1)*t*t*t - 1) + b;
        }
+
     function cssTest(name){
             var prop=camelCase(name),
                 _prop=camelCase(cssVendor+prop);
@@ -42,3 +43,45 @@ cssVendor=function(){
                         }catch(e){}
                     });
                 }
+
+var throttle = function(func, wait, options) {
+    var timeout, context, args, result;
+    var previous = 0;
+    if (!options) options = {};
+
+    var later = function() {
+        previous = options.leading === false ? 0 : _.now();
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+    };
+
+    var throttled = function() {
+        var now = _.now();
+        if (!previous && options.leading === false) previous = now;
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            previous = now;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+        } else if (!timeout && options.trailing !== false) {
+            timeout = setTimeout(later, remaining);
+        }
+        return result;
+    };
+
+    throttled.cancel = function() {
+        clearTimeout(timeout);
+        previous = 0;
+        timeout = context = args = null;
+    };
+
+    return throttled;
+};
+
